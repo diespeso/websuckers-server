a6c192cf5d28d037f9c723d92176ce32990b1c7d
edgar_regalado <edgar.regalado@g-global.com>
2022-08-21 23:55:36 -0700

    server working ok

---
+++ server.js
@@ -0,0 +1,107 @@
+const os = require('os');
+
+const dotenv = require('dotenv');
+const express = require('express');
+const expressWs = require('express-ws');
+const { v4: uuidv4 } = require('uuid');
+const { WebSocket } = require('ws');
+
+
+dotenv.config();
+
+const IP = os.networkInterfaces().Ethernet[0].address; // change this if the network changes to wifi
+const DEBUG = true;
+const debug = (text) => { if (DEBUG) {console.log(text);} }
+
+const app = express();
+const wsServer = expressWs(app);
+
+let clients = {};
+
+// COMMON STARTS 
+const MESSAGE_TYPES = {
+    GREET: 'GREET',
+    GRANT_IDENTIFIER: 'GRANT_IDENTIFIER',
+    TEXT: 'TEXT',
+    BROADCAST: 'BROADCAST',
+};
+
+const sendJson = (ws, json) => (ws.send(JSON.stringify(json)));
+
+const receiveJson = (message) => {
+    let received;
+    try {
+        received = JSON.parse(message);
+    } catch (e) {
+        if (e.name === 'SyntaxError') {
+            console.log('failed to parse received data as JSON: bad format');
+        }
+    }
+    return received;
+};
+
+const buildMessage = (messageType, messageObject) => ({
+    messageType,
+    ...messageObject
+});
+
+const sendBroadcast = (ws, data) => {
+    console.log(Object.keys(clients));
+    Object.keys(clients).forEach((clientUuid) => {
+        if (data.id !== clientUuid) { //everyone but the emissor
+            sendMessage(clients[clientUuid], MESSAGE_TYPES.BROADCAST, { broadcast: data.broadcast, id: data.id});
+        }
+    });
+}
+
+const handleMessage = (ws, req, data) => {
+    debug(data);
+    switch(data.messageType) {
+        case MESSAGE_TYPES.GREET:
+            const id = uuidv4();
+            clients[id] = ws;
+            sendGrantedIdentifier(ws, id);
+            break;
+        case MESSAGE_TYPES.TEXT:
+            debug(`user ${data.id} says: ${data.text}`);
+            break;
+        case MESSAGE_TYPES.BROADCAST:
+            debug(`user ${data.id} broadcasts: ${data.broadcast}`);
+            sendBroadcast(ws, data);
+            break;
+    }
+};
+
+const sendMessage = (ws, messageType, messageObject) => {
+    sendJson(ws, buildMessage(messageType, messageObject));
+}
+
+// COMMON ENDS
+
+const sendGrantedIdentifier = (ws, id) => {
+    sendMessage(ws, MESSAGE_TYPES.GRANT_IDENTIFIER, { id });
+};
+
+const checkGreeting = (data) => {
+    const currentUuids = Object.keys(clients);
+    return !currentUuids.includes(data.id);
+}
+
+// routing
+app.ws('/', (ws, req) => {
+    console.log('client connects');
+    ws.on('message', (message) => {
+        const data = receiveJson(message);
+        if (data) {
+            handleMessage(ws, req, data);
+        }
+    });
+
+    ws.on('close', () => {
+        clients = {};
+    })
+
+});
+
+
+app.listen(process.env.PORT, () => { console.log(`ws server listening at ${IP}:${process.env.PORT}`); });
